d9dbc7e257a500900aa5fc226fc451af32789169
YOLORabbit <surprise.eng000@gmail.com>
2025-01-27 23:07:51 +0200

    Initialize subgraph

--- exchange-v3/src/pancake-v-3-factory.ts
+++ exchange-v3/src/pancake-v-3-factory.ts
@@ -1,75 +1,105 @@
-import { BigInt, Bytes } from "@graphprotocol/graph-ts"
 import {
-  PancakeV3Factory,
+  FeeAmountEnabled as FeeAmountEnabledEvent,
+  FeeAmountExtraInfoUpdated as FeeAmountExtraInfoUpdatedEvent,
+  OwnerChanged as OwnerChangedEvent,
+  PoolCreated as PoolCreatedEvent,
+  SetLmPoolDeployer as SetLmPoolDeployerEvent,
+  WhiteListAdded as WhiteListAddedEvent
+} from "../generated/PancakeV3Factory/PancakeV3Factory"
+import {
   FeeAmountEnabled,
   FeeAmountExtraInfoUpdated,
   OwnerChanged,
   PoolCreated,
   SetLmPoolDeployer,
   WhiteListAdded
-} from "../generated/PancakeV3Factory/PancakeV3Factory"
-import { ExampleEntity } from "../generated/schema"
+} from "../generated/schema"
 
-export function handleFeeAmountEnabled(event: FeeAmountEnabled): void {
-  // Entities can be loaded from the store using an ID; this ID
-  // needs to be unique across all entities of the same type
-  const id = event.transaction.hash.concat(
-    Bytes.fromByteArray(Bytes.fromBigInt(event.logIndex))
+export function handleFeeAmountEnabled(event: FeeAmountEnabledEvent): void {
+  let entity = new FeeAmountEnabled(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  let entity = ExampleEntity.load(id)
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(id)
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
   entity.fee = event.params.fee
   entity.tickSpacing = event.params.tickSpacing
 
-  // Entities can be written to the store with `.save()`
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
   entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.collectProtocol(...)
-  // - contract.createPool(...)
-  // - contract.feeAmountTickSpacing(...)
-  // - contract.feeAmountTickSpacingExtraInfo(...)
-  // - contract.getPool(...)
-  // - contract.lmPoolDeployer(...)
-  // - contract.owner(...)
-  // - contract.poolDeployer(...)
 }
 
 export function handleFeeAmountExtraInfoUpdated(
-  event: FeeAmountExtraInfoUpdated
-): void {}
+  event: FeeAmountExtraInfoUpdatedEvent
+): void {
+  let entity = new FeeAmountExtraInfoUpdated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.fee = event.params.fee
+  entity.whitelistRequested = event.params.whitelistRequested
+  entity.enabled = event.params.enabled
 
-export function handleOwnerChanged(event: OwnerChanged): void {}
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
 
-export function handlePoolCreated(event: PoolCreated): void {}
+  entity.save()
+}
 
-export function handleSetLmPoolDeployer(event: SetLmPoolDeployer): void {}
+export function handleOwnerChanged(event: OwnerChangedEvent): void {
+  let entity = new OwnerChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.oldOwner = event.params.oldOwner
+  entity.newOwner = event.params.newOwner
 
-export function handleWhiteListAdded(event: WhiteListAdded): void {}
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handlePoolCreated(event: PoolCreatedEvent): void {
+  let entity = new PoolCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.token0 = event.params.token0
+  entity.token1 = event.params.token1
+  entity.fee = event.params.fee
+  entity.tickSpacing = event.params.tickSpacing
+  entity.pool = event.params.pool
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleSetLmPoolDeployer(event: SetLmPoolDeployerEvent): void {
+  let entity = new SetLmPoolDeployer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.lmPoolDeployer = event.params.lmPoolDeployer
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleWhiteListAdded(event: WhiteListAddedEvent): void {
+  let entity = new WhiteListAdded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.user = event.params.user
+  entity.verified = event.params.verified
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
--- exchange-v3/tests/pancake-v-3-factory.test.ts
+++ exchange-v3/tests/pancake-v-3-factory.test.ts
@@ -7,8 +7,8 @@ import {
   afterAll
 } from "matchstick-as/assembly/index"
 import { Address } from "@graphprotocol/graph-ts"
-import { ExampleEntity } from "../generated/schema"
-import { FeeAmountEnabled } from "../generated/PancakeV3Factory/PancakeV3Factory"
+import { FeeAmountEnabled } from "../generated/schema"
+import { FeeAmountEnabled as FeeAmountEnabledEvent } from "../generated/PancakeV3Factory/PancakeV3Factory"
 import { handleFeeAmountEnabled } from "../src/pancake-v-3-factory"
 import { createFeeAmountEnabledEvent } from "./pancake-v-3-factory-utils"
 
@@ -30,19 +30,19 @@ describe("Describe entity assertions", () => {
   // For more test scenarios, see:
   // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
 
-  test("ExampleEntity created and stored", () => {
-    assert.entityCount("ExampleEntity", 1)
+  test("FeeAmountEnabled created and stored", () => {
+    assert.entityCount("FeeAmountEnabled", 1)
 
     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
     assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "FeeAmountEnabled",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "fee",
       "123"
     )
     assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "FeeAmountEnabled",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "tickSpacing",
       "123"
     )
